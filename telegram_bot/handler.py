6586a2d1ba67fdc2c2689b72cedfa62a2ad9025c
Riccardo <riki-95@hotmail.it>
2019-02-18 15:38:40 +0100

    Added the menu to /rooms

--- telegram_bot/handler.py
+++ telegram_bot/handler.py
@@ -1,37 +1,17 @@
 import json
 import os
 import sys
+import aws_db as db
 
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, "./vendored"))
-import boto3
 import requests
-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Bot
 
 TOKEN = os.environ['TELEGRAM_TOKEN']
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
-
-dynamodb = boto3.resource('dynamodb')
-
-
-def scan_table(table_name):
-    table = dynamodb.Table(table_name)
-    
-    items = table.scan()
-    return items['Items']
-
-
-def insert_table(table_name, room, status):
-    table = dynamodb.Table(table_name)
-    
-    item = {
-        'room': room,
-        'status': status
-    }
-    
-    table.put_item(Item=item)
-
+bot = Bot(TOKEN)
 
 def format_items(items):
     res = ''
@@ -60,24 +40,33 @@ def handler(event, context):
         chat_id = data["message"]["chat"]["id"]
 
         if '/scan' in message:
-            response = format_items(scan_table('uc_db'))
+            response = format_items(db.scan_table('uc_db'))
         elif '/add' in message:
             mess_args = message.split()
             r = mess_args [1]
             response = r
         elif '/rooms' in message:
-            response = build_menu(['uno', 'due'], 2, None, None)
+            button_list = [
+                InlineKeyboardButton("col1", callback_data='uno'),
+                InlineKeyboardButton("col2", callback_data='due'),
+                InlineKeyboardButton("row 2", callback_data='tre')
+            ]
+            reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
+            bot.send_message(chat_id, "A two-column menu", reply_markup=reply_markup)
+            
         else:
             response = 'hello' #json.dumps(event)
 
         print('res', response)
 
-        data = {"text": response.encode("utf8"), "chat_id": chat_id}
-        url = BASE_URL + "/sendMessage"
-        requests.post(url, data)
+        
 
     except Exception as e:
-        print('exception:', e)
+        response = str(e)
     
+    data = {"text": response.encode("utf8"), "chat_id": chat_id}
+    url = BASE_URL + "/sendMessage"
+    requests.post(url, data)
+
     print('=== end ===')
     return {"statusCode": 200}
