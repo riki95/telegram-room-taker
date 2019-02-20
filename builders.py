3ef5f6723540407300dbf9a45c440d643b91f684
Riccardo <riki-95@hotmail.it>
2019-02-20 09:53:46 +0100

    Created builders.py + Refactoring

---
+++ builders.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, './vendored'))
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Bot
+
+def format_items(items):
+    res = ''
+    for item in items:
+        res += 'Room {item[room]}: {status}\n'.format(
+            item=item,
+            status='occupied' if item['occupied'] else 'free')
+    return res
+
+
+def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    reply_markup = InlineKeyboardMarkup(menu)
+    return reply_markup
+
+
+def button_list(rooms, status):
+    return [
+        InlineKeyboardButton(
+            'Room {}'.format(room['room']),
+            callback_data='room {} {}'.format(room['room'], status)
+        ) for room in rooms
+    ]
--- telegram_bot/handler.py
+++ telegram_bot/handler.py
@@ -4,44 +4,17 @@ import sys
 import traceback
 
 import aws_db as db
+import builders as builder
 
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, './vendored'))
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Bot
+from telegram import Bot
 
 
 TOKEN = os.environ['TELEGRAM_TOKEN']
 BASE_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 
-
 bot = Bot(TOKEN)
-
-
-def format_items(items):
-    res = ''
-    for item in items:
-        res += 'Room {item[room]}: {status}\n'.format(
-            item=item,
-            status='occupied' if item['occupied'] else 'free')
-    return res
-
-
-def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons)
-    if footer_buttons:
-        menu.append(footer_buttons)
-    return menu
-
-
-def button_list(rooms, status):
-    return [
-        InlineKeyboardButton(
-            'Room {}'.format(room['room']),
-            callback_data='room {} {}'.format(room['room'], status)
-        ) for room in rooms
-    ]
 
 
 def handle_cb_room(data):
@@ -70,16 +43,14 @@ def handler_cb(data):
 def handle_status(data):
     chat_id = data['message']['chat']['id']
 
-    bot.send_message(chat_id, format_items(db.scan_table()))
+    bot.send_message(chat_id, builder.format_items(db.scan_table()))
 
 
 def handle_room(data, action):
     chat_id = data['message']['chat']['id']
 
-    buttons = button_list(db.scan_table(), action)
-    menu = build_menu(buttons, n_cols=2)
-
-    reply_markup = InlineKeyboardMarkup(menu)
+    buttons = builder.button_list(db.scan_table(), action)
+    reply_markup = builder.build_menu(buttons, n_cols=2)
 
     bot.send_message(chat_id, 'Take a room:', reply_markup=reply_markup)
 
